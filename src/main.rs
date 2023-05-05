93d9bd8dd7ece173b762a0ab51d1c19f6150249f
GeckoSplat <111183176+GeckoSplat@users.noreply.github.com>
2023-05-05 01:13:39 +0100

    Checks for exif data first, parsed for printing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 fn main() {
     extract_exif();
 }
@@ -8,6 +7,7 @@ use regex::Regex;
 use std::env;
 use std::fs::File;
 use std::io::BufReader;
+use std::os::fd::AsFd;
 use std::path::{Path, PathBuf};
 
 use exif::{In, Tag};
@@ -15,33 +15,46 @@ fn extract_exif() -> Result<(), exif::Error> {
     let folder_path = std::fs::read_dir("./pictures").expect("file import ERROR");
 
     for pic in folder_path {
+        println!("{:?}", pic.as_ref().unwrap().path().file_name().unwrap());
         let file = std::fs::File::open(pic.unwrap().path().as_path())?;
-        println!("{:?}", file);
 
         let mut bufreader = std::io::BufReader::new(&file);
-        // println!("{:?}",bufreader);
+
         let exifreader = exif::Reader::new();
         let exif = exifreader.read_from_container(&mut bufreader).unwrap();
 
-        let gps_lat = exif.get_field(Tag::GPSLatitude, In::PRIMARY).unwrap(); // problem here if data absent
-        let gps_long = exif.get_field(Tag::GPSLongitude, In::PRIMARY).unwrap();
+        let gps_lat = exif.get_field(Tag::GPSLatitude, In::PRIMARY);
+        let gps_long = exif.get_field(Tag::GPSLongitude, In::PRIMARY);
 
-        let print_lat = gps_lat.display_value().with_unit(&exif).to_string();
-        let print_long = gps_long.display_value().with_unit(&exif).to_string();
+        if gps_lat.is_none() {
+            println!("File contains no Exif Data")
+        } else {
+            let print_lat = gps_lat
+                .expect("NO DATA")
+                .display_value()
+                .with_unit(&exif)
+                .to_string();
+            let print_long = gps_long
+                .expect("NO DATA")
+                .display_value()
+                .with_unit(&exif)
+                .to_string();
 
-        let reg_exclude = Regex::new(r"[a-z]").unwrap();
+            let reg_exclude = Regex::new(r"[a-z]").unwrap();
 
-        let reg_replace_empty_str_lat = reg_exclude.replace_all(&print_lat, "");
-        let reg_replace_empty_str_long = reg_exclude.replace_all(&print_long, "");
+            let reg_replace_empty_str_lat = reg_exclude.replace_all(&print_lat, "");
+            let reg_replace_empty_str_long = reg_exclude.replace_all(&print_long, "");
 
-        println!(
-            "Latitude,Longitude: {} {}",
-            reg_replace_empty_str_lat, reg_replace_empty_str_long
-        );
+            println!(
+                "Latitude,Longitude: {} {}",
+                reg_replace_empty_str_lat, reg_replace_empty_str_long
+            );
+        }
     }
+
     Ok(())
 }
 
-// Todo : negate empty data at start , match ? 
+// Todo :
 //        parse to load straight to google map .
-//        print to terminal, print to file ? 
+//        print to terminal, print to file ?
