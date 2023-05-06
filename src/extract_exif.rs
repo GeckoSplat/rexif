903cd935f6dccf9d733e0e4a95212daef7d21657
GeckoSplat <111183176+GeckoSplat@users.noreply.github.com>
2023-05-07 00:23:23 +0100

    unwraps changed to expects

--- src/extract_exif.rs
+++ src/extract_exif.rs
@@ -1,18 +1,18 @@
 extern crate exif;
-use regex::Regex;
 use exif::{In, Tag};
+use regex::Regex;
 
-pub fn extract_exif() -> Result<(), exif::Error> {
+pub fn extract_exif() {
     let folder_path = std::fs::read_dir("./pictures").expect("file import ERROR");
 
     for pic in folder_path {
         println!("{:?}", pic.as_ref().unwrap().path().file_name().unwrap());
-        let file = std::fs::File::open(pic.unwrap().path().as_path())?;
+        let file = std::fs::File::open(pic.unwrap().path().as_path()).expect("file ooops");
 
         let mut bufreader = std::io::BufReader::new(&file);
 
         let exifreader = exif::Reader::new();
-        let exif = exifreader.read_from_container(&mut bufreader).unwrap();
+        let exif = exifreader.read_from_container(&mut bufreader).expect("exif read ooops");
 
         let gps_lat = exif.get_field(Tag::GPSLatitude, In::PRIMARY);
         let gps_long = exif.get_field(Tag::GPSLongitude, In::PRIMARY);
@@ -31,7 +31,7 @@ pub fn extract_exif() -> Result<(), exif::Error> {
                 .with_unit(&exif)
                 .to_string();
 
-            let reg_exclude = Regex::new(r"[a-z]").unwrap();
+            let reg_exclude = Regex::new(r"[a-z]").expect("regex oops");
 
             let reg_replace_empty_str_lat = reg_exclude.replace_all(&print_lat, "");
             let reg_replace_empty_str_long = reg_exclude.replace_all(&print_long, "");
@@ -48,6 +48,4 @@ pub fn extract_exif() -> Result<(), exif::Error> {
             println!("{for_google}\n");
         }
     }
-
-    Ok(())
 }
