e758b8c59de91ca8348f82a683502514162b7c4b
GeckoSplat <111183176+GeckoSplat@users.noreply.github.com>
2023-05-10 22:37:34 +0100

    prints working hyperlink

--- src/extract_exif.rs
+++ src/extract_exif.rs
@@ -1,9 +1,8 @@
 extern crate exif;
-use exif::{In, Tag};
-use regex::Regex;
+use exif::{In, Tag, Value};
 
 pub fn extract_exif() {
-    let folder_path = std::fs::read_dir("./pictures").expect("file import ERROR");
+    let folder_path = std::fs::read_dir("./pictures").expect("Directory read ERROR");
 
     for pic in folder_path {
         println!("{:?}", pic.as_ref().unwrap().path().file_name().unwrap());
@@ -14,38 +13,70 @@ pub fn extract_exif() {
         let exifreader = exif::Reader::new();
         let exif = exifreader.read_from_container(&mut bufreader).expect("exif read ooops");
 
-        let gps_lat = exif.get_field(Tag::GPSLatitude, In::PRIMARY);
-        let gps_long = exif.get_field(Tag::GPSLongitude, In::PRIMARY);
+        let gps_check_data = exif.get_field(Tag::GPSLatitude, In::PRIMARY);
 
-        if gps_lat.is_none() {
+        if gps_check_data.is_none() {
             println!("File contains no Exif Data")
         } else {
-            let print_lat = gps_lat
-                .expect("NO DATA")
+            let gps_lat = &exif
+            .get_field(Tag::GPSLatitude, In::PRIMARY)
+            .unwrap()
+            .display_value()
+            .with_unit(&exif);
+
+            let gps_long = exif
+                .get_field(Tag::GPSLongitude, In::PRIMARY)
+                .unwrap()
                 .display_value()
-                .with_unit(&exif)
-                .to_string();
-            let print_long = gps_long
-                .expect("NO DATA")
-                .display_value()
-                .with_unit(&exif)
-                .to_string();
+                .with_unit(&exif);
 
-            let reg_exclude = Regex::new(r"[a-z]").expect("regex oops");
 
-            let reg_replace_empty_str_lat = reg_exclude.replace_all(&print_lat, "");
-            let reg_replace_empty_str_long = reg_exclude.replace_all(&print_long, "");
+        // latitude first , then longitude for maps.
+        // match statements combine the string needed to load straight to google maps in print!.
 
-            let for_google = format!(
-                "www.google.com/maps/place/{}{}",
-                reg_replace_empty_str_lat, reg_replace_empty_str_long
-            );
+        match exif.get_field(Tag::GPSLatitude, In::PRIMARY) {
+            Some(xres) => match xres.value {
+                Value::Rational(ref v) if !v.is_empty() => {
+                    if gps_lat.to_string().ends_with("W") {
+                        let lodeg = v[0].to_f64() + v[1].to_f64() / 60.0 + v[2].to_f64() / 3600.0;
 
-            println!(
-                "Latitude,Longitude: {} {}",
-                reg_replace_empty_str_lat, reg_replace_empty_str_long
-            );
-            println!("{for_google}\n");
+                        print!("https://maps.google.com/?q={:?},", lodeg * -1.0);
+                    } else if gps_lat.to_string().ends_with("S") {
+                        let lodeg = v[0].to_f64() + v[1].to_f64() / 60.0 + v[2].to_f64() / 3600.0;
+                        print!("https://maps.google.com/?q={:?},", lodeg * -1.0);
+                    } else {
+                        let lodeg = v[0].to_f64() + v[1].to_f64() / 60.0 + v[2].to_f64() / 3600.0;
+
+                        print!("https://maps.google.com/?q={:?},", lodeg);
+                    }
+                }
+
+                _ => eprintln!("Latitude value is broken"),
+            },
+
+            None => eprintln!("Latitude tag is missing"),
+        }
+
+        match exif.get_field(Tag::GPSLongitude, In::PRIMARY) {
+            Some(xres) => match xres.value {
+                Value::Rational(ref v) if !v.is_empty() => {
+                    if gps_long.to_string().ends_with("W") {
+                        let lodeg = v[0].to_f64() + v[1].to_f64() / 60.0 + v[2].to_f64() / 3600.0;
+                        println!("{:?}", lodeg * -1.0)
+                    } else if gps_long.to_string().ends_with("S") {
+                        let lodeg = v[0].to_f64() + v[1].to_f64() / 60.0 + v[2].to_f64() / 3600.0;
+                        println!("{:?}", lodeg * -1.0)
+                    } else {
+                        let lodeg = v[0].to_f64() + v[1].to_f64() / 60.0 + v[2].to_f64() / 3600.0;
+                        println!("{:?}", lodeg)
+                        
+                    }
+                }
+
+                _ => eprintln!("Longitude value is broken"),
+            },
+            None => eprintln!("Longitude tag is missing"),
+        }
         }
     }
 }
